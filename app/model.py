f4dbbe8706135930a726acbfd79567cef5f5dad8
Mishab <mishabmaruthil321@gmail.com>
2025-12-14 19:09:21 +0530

    fixed model pylint score issue

--- app/model.py
+++ app/model.py
@@ -4,11 +4,11 @@ from sklearn.linear_model import LogisticRegression
 
 def train_model():
     iris = load_iris()
-    X = iris.data
-    y = iris.target
+    features = iris.data  # pylint: disable=no-member
+    labels = iris.target  # pylint: disable=no-member
 
     model = LogisticRegression(max_iter=200)
-    model.fit(X, y)
+    model.fit(features , labels )
 
     return model
 
